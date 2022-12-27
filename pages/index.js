d6067ba3239fb8d9d1bcfb9bc0dcabaef9d7785f
Travis Kohlbeck <me+git@travisk.info>
2022-12-27 04:44:00 -0500

    Further attempt svg font fix

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <Image
+        <img
           src="/homaro-logo.svg"
           alt="Homaro Logo"
           width={300}
