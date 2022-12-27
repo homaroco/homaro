60c1090e7a5bbad9f04e0f64e12e36c9275462c7
Travis Kohlbeck <me+git@travisk.info>
2022-12-27 04:26:10 -0500

    Attempt svg font fix

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Nunito } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.header}>
+      <header className={`${styles.header} ${nunito.className}`}>
         <Image
           src="/homaro-logo.svg"
           alt="Homaro Logo"
