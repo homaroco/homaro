26e8106a34d485fc76e4f86711c819defb94bd25
Travis Kohlbeck <me+git@travisk.info>
2022-12-29 22:04:48 -0500

    Fix typos, fix styling

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
       </header>
       <main className={styles.main}>
         <h3>{"This company does not yet exist, but it will soon. Here's the gist"}</h3>
-        <p>While there are a number of goals that Homaro seeks to acomplish (enumerated upon in the <a href='#footnotes'>footnotes</a>) there are three goals in particular that will chart the initial company course. In order of increasing importance:</p>
+        <p>While there are a number of goals that Homaro seeks to accomplish (enumerated upon in the <a href='#footnotes'>footnotes</a>) there are three goals in particular that will chart the initial company course. In order of increasing importance:</p>
         <ul>
           <li><a href="#privacy-and-security">Consumer privacy and security</a></li>
           <li><a href="#market-expansion">Benefit-focused market expansion</a></li>
@@ -51,16 +51,17 @@ export default function Home() {
         </h3>
         <p>Web-based software will always play a major role in Homaro products and services, due to its ease of access and ability to quickly build and innovate. But the point is not to create software, it is to produce benefit wherever the opportunity is found. By spreading awareness of Homaro&apos;s mission, building consumer trust, and never—ever—violating that trust, we hope to jump into new markets, each time an opportunity for the company to prove itself trustworthy, and each time a little bit easier than the last.</p>
         <p>How will we build public trust? By treating people with respect wherever possible. This will involve privacy policies and terms of service that are simple and short, <a href="#compensation">egalitarian employee compensation,<sup>[2]</sup></a> informed-consent op-in features, truth in advertising, and so on.</p>
-        <p>In which markets will we choose to expand? Initially, wherever a software solution offers low-hanging fruit, and wherever consumers are treated with disrespect. This should provide ample opportunity to grow the company. Potential avenues may include open-source voting software, tools or services for local journalism, event planning for charity organizations and other non-profit groups, services to support small farms, developing-world (or developed) microlending, mobile pet spaying, etc.</p>
+        <p>In which markets will we choose to expand? Initially, wherever a software solution offers low-hanging fruit, and wherever consumers are treated with disrespect. This should provide ample opportunity to grow the company. Potential avenues may include open-source voting software, tools or services for local journalism, event planning for charity organizations and other non-profit groups, services to support small farms, developing-world (or developed) micro-lending, mobile pet neutering, password and secret management, etc.</p>
         <h3 id='democracy'>
           <Link href="/#democracy">
             Democratization of corporate governance
           </Link>
         </h3>
         <p>{"Homaro will be an experiment in corporate governance. Policy and direction of a typical public company is voted on by its shareholders, with each shareholder's vote being proportional to how much stock they own (commonly known as the \"one share, one vote\" principle). In addition to a group having \"one share, one vote\", Homaro will have a second group following a \"one shareholder, one vote\" principle, wherein each partial owner of the company has equal voting power, regardless of how many shares they own."}</p>
-        <p>{"In addion to the shareholders, a public benefit company must also take into account its stakeholders. Therefore, in addition to the two share(holder) groups, there will be two stake(holder) groups as well. The first will consist of every registered user of Homaro products or services, each having an equal vote. The second will consist of every registered user who has paid for Homaro products or services, each having a vote proportional to how much they have paid. A majority vote from all four groups will be required for policy to be passed."}</p>
-        <p>{"As much power as possible will be vested in these four voting groups. They will elect the chief officers, they will elect boards of directors, and they will have the ability to dissolve the company should it be deemd unable to accomplish its mission."}</p>
-        <p>{"There are obvious drawbacks to such a system, perhaps the most obvious being a reduced ability to innovate, or quickly respond to change. While there are ways to mitigate this issue (such as making policy proposal and voting available online with automatic deadlines), the point of Homaro is not to maximize profit, but rather place as much power in the hands of ordinary people as possible. The results should speak for themselves."}</p>
+        <p>{"A public benefit company must also take into account its stakeholders. Therefore, in addition to the two share(holder) groups, there will be two stake(holder) groups as well. The first will consist of every registered user of Homaro products or services, each having an equal vote. The second will consist of every registered user who has paid for Homaro products or services, each having a vote proportional to how much they have paid."}</p>
+        <p>Policy proposals must pass in at least three of the four groups (a majority of majorities) in order to become company policy. Some major decisions may require all four groups.</p>
+        <p>{"As much power as possible will be vested in these four voting groups. They will elect company leadership, they will make major policy decisions, and they will have the ability to dissolve the company should it be deemed unable to accomplish its mission."}</p>
+        <p>{"There are obvious drawbacks to such a system, perhaps the most obvious being a reduced ability to innovate, or quickly respond to change. While there are ways to mitigate this issue (such as making policy proposal and voting available online with automatic deadlines), the point of Homaro is not to maximize profit, but rather place as much power in the hands of ordinary people as possible. We believe the results will speak for themselves."}</p>
         <h3 id="summary">
           <Link href="/#summary">
             In summary
@@ -83,7 +84,7 @@ export default function Home() {
         <h4>If I&apos;m a shareholder who pays for company products or services, in which group will I have a vote?</h4>
         <p>All of them! Your vote on policy proposals will count four times, though with varying weight.</p>
         <h4>Where can I reach you?</h4>
-        <p>If you have questions, concerns, feedback, or interest in participating in this expierment, please reach out to <a href="mailto:info@homaro.co">info at homaro.co</a>. We can&apos;t do this without the help of others, and we hope in the kindness of strangers.</p>
+        <p>If you have questions, concerns, feedback, or interest in participating in this experiment, please reach out to <a href="mailto:info@homaro.co">info at homaro.co</a>. We can&apos;t do this without the help of others, and we hope in the kindness of strangers.</p>
 
         {/* <p>{"It's time to do something revolutionary, and I hope that you take part."}</p> */}
         <h3 id="footnotes">
